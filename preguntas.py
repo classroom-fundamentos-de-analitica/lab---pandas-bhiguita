0dcd087a8a5948ffd0277e41cef08204fd102955
bhiguita <125924883+bhiguita@users.noreply.github.com>
2023-06-13 22:20:13 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -4,7 +4,6 @@ Laboratorio - Manipulación de Datos usando Pandas
 Este archivo contiene las preguntas que se van a realizar en el laboratorio.
 Utilice los archivos `tbl0.tsv`, `tbl1.tsv` y `tbl2.tsv`, para resolver las preguntas.
 """
-pip install pandas
 
 import pandas as pd
 import numpy as np
